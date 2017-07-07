60d4932bbe98120bf588d1cb0cc7b688b3daec93
anandpushkar <pa@spade99>
2017-07-07 18:56:57 +0530

    2nd commit

--- app/src/main/java/com/pushkaranand/restapiretrofitjsonadvance/models/Album.kt
+++ app/src/main/java/com/pushkaranand/restapiretrofitjsonadvance/models/Album.kt
@@ -4,7 +4,6 @@ package com.pushkaranand.restapiretrofitjsonadvance.models
  * Created by Pushkar on 03-Jul-17.
  */
 data class Album (
-
         val id: Int,
         val userId: Int,
         val title: String
