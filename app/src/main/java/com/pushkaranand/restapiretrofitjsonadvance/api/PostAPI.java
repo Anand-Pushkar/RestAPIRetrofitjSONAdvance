957d628fa08f37a50faa40baab319ca8bf44fc28
anandpushkar <pa@spade99>
2017-07-08 17:13:43 +0530

    3rd commit

--- app/src/main/java/com/pushkaranand/restapiretrofitjsonadvance/api/PostAPI.java
+++ app/src/main/java/com/pushkaranand/restapiretrofitjsonadvance/api/PostAPI.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -30,6 +31,4 @@ public interface PostAPI {
             @Nullable
             @Query("userId") Integer userId
     );
-
-
-  }
+}
